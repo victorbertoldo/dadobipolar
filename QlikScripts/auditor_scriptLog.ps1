080ca82f3956445368a6ec800373f5e52fa40036
victor.bertoldo <eu_viktor@hotmail.com>
2019-06-26 14:43:51 -0300

    Incremento da descrição

--- QlikScripts/auditor_scriptLog.ps1
+++ QlikScripts/auditor_scriptLog.ps1
@@ -1,4 +1,11 @@
-﻿$var = Get-ChildItem -Path "C:\Users\victor.bertoldo\Documents\Qlik\Sense\Log\Script\" |  Sort-Object -Property CreationTime | select -Last 1 
+﻿################################################################################################################################################
+# Após a instalação da Extensão PowerShell da Microsoft, basta selecionar o Script e teclar F8 para fazer o trace em tempo real.
+#
+################################################################################################################################################
+## A variável que está descomentada, está apontando para o diretorio local no Sense Desktop
+$var = Get-ChildItem -Path "C:\Users\victor.bertoldo\Documents\Qlik\Sense\Log\Script\" |  Sort-Object -Property CreationTime | select -Last 1 
+#
+################################################################################################################################################
 #O diretório comentado é um mapeamento da pasta de logs do servidor
 #$var = Get-ChildItem -Path "S:\" |  Sort-Object -Property CreationTime | select -Last 1 
-get-content $var.FullName -Wait
+    get-content $var.FullName -Wait
