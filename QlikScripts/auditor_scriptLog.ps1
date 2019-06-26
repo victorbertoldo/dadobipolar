d5078e42c779da5d88340962a84b456888ed6000
victor.bertoldo <eu_viktor@hotmail.com>
2019-06-26 14:50:58 -0300

    Correção proposta pelo Script Analyser do PwS

--- QlikScripts/auditor_scriptLog.ps1
+++ QlikScripts/auditor_scriptLog.ps1
@@ -3,9 +3,9 @@
 #
 ################################################################################################################################################
 ## A variável que está descomentada, está apontando para o diretorio local no Sense Desktop
-$var = Get-ChildItem -Path "C:\Users\victor.bertoldo\Documents\Qlik\Sense\Log\Script\" |  Sort-Object -Property CreationTime | select -Last 1 
+$var = Get-ChildItem -Path "C:\Users\victor.bertoldo\Documents\Qlik\Sense\Log\Script\" |  Sort-Object -Property CreationTime | Select-Object -Last 1 
 #
 ################################################################################################################################################
 #O diretório comentado é um mapeamento da pasta de logs do servidor
-#$var = Get-ChildItem -Path "S:\" |  Sort-Object -Property CreationTime | select -Last 1 
+#$var = Get-ChildItem -Path "S:\" |  Sort-Object -Property CreationTime | Select-Object -Last 1 
     get-content $var.FullName -Wait
