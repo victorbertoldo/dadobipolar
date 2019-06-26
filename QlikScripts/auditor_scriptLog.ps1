08763b4ada69ccad1a961cf4563f04564185cd6c
victor.bertoldo <eu_viktor@hotmail.com>
2019-06-26 14:36:17 -0300

    Criação de PowerShell Script para auditoria de Logs

---
+++ QlikScripts/auditor_scriptLog.ps1
@@ -0,0 +1,4 @@
+﻿$var = Get-ChildItem -Path "C:\Users\victor.bertoldo\Documents\Qlik\Sense\Log\Script\" |  Sort-Object -Property CreationTime | select -Last 1 
+#O diretório comentado é um mapeamento da pasta de logs do servidor
+#$var = Get-ChildItem -Path "S:\" |  Sort-Object -Property CreationTime | select -Last 1 
+get-content $var.FullName -Wait
