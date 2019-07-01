cf89c4e050b2734cf86c42b6ebae45a9eec6e8d7
victor.bertoldo <eu_viktor@hotmail.com>
2019-07-01 16:45:41 -0300

    Criação do script de transf o qvf para o repositorio, e alterações de organização

---
+++ Utils/get-qvf.ps1
@@ -0,0 +1,14 @@
+﻿<#
+Copyright (c) 2019 Victor Bertoldo
+
+Este script importa o qvf para o repositório git configurado
+
+Criar a variável de ambiente de sistema: qlikPath com o valor %userprofile%\Documents\Qlik\Sense\Apps\
+#>
+$path       = $env:qlikPath
+$app        = "Zabbix.qvf" # Digite o nome do App
+$repo       =  "D:\dadobipolar\Zabbix\" #Coloque o caminho destino, onde o repositório local foi configurado.
+
+
+
+(New-Object -ComObject Scripting.FileSystemObject).CopyFile($path+$app, $repo)
