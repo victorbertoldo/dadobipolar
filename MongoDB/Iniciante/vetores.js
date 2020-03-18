caf1a1f66614f46f3d052a5919c155b07b881862
Victor Bertoldo <eu_viktor@hotmail.com>
2020-03-18 11:13:25 -0300

    Mongo

--- MongoDB/Iniciante/vetores.js
+++ MongoDB/Iniciante/vetores.js
@@ -4,4 +4,24 @@ db.funcionarios.insertOne(
         idade: 29,
         filhos: ['Mariana','Raul']
     }
-)
+);
+
+db.funcionarios.find().pretty();
+
+db.funcionarios.insertMany([
+    {
+        nome: 'João',
+        idade: 29,
+        filhos: ['Mariana','Raul']
+    },
+    {
+        nome: 'Flávia',
+        idade: 29,
+        filhos: ['Alan','José']
+    },
+    {
+        nome: 'Livia',
+        idade: 29,
+        filhos: ['Laura','Eva']
+    }
+])
